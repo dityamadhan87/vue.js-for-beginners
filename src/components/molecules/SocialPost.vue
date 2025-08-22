6ef3724b5f2bc77a3f1aac280238c2116ced4261
Raditya Rahmadhani <edogawaradit@gmail.com>
2025-08-22 10:18:12 +0700

    Refactor App.vue to use TheHeader component and remove unused code; enhance TheWelcome and SocialPost components with additional post properties; implement SocialPostComments component for displaying comments.

--- src/components/molecules/SocialPost.vue
+++ src/components/molecules/SocialPost.vue
@@ -2,27 +2,60 @@
   <div
     class="SocialPost"
     :class="{ SocialPost__selected: selected}"
-    @click="selected = !selected"
+    @click="onSelectedClick"
   >
     <div class="header">
       <img class="avatar" :src="avatarSrc" />
       <div class="name">{{ username }}</div>
       <div class="userId">{{ userId }}</div>
     </div>
-    <div class="post">{{ post }}</div>
+    <div class="post" v-text="post"></div>
+    <button
+      v-show="hasComments"
+      @click="onShowCommentClick"
+    >
+      Show Comments
+    </button>
+    <SocialPostComments
+      v-if="showComments"
+      :comments="comments"
+    />
+    <div class="interactions">Interactions: {{ interactions }}</div>
   </div>
 </template>
 
 <script setup >
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
+import SocialPostComments from './SocialPostComments.vue';
 
 const selected = ref(false);
+const onSelectedClick = () => {
+  selected.value = !selected.value;
+}
 
+const showComments = ref(false);
+const onShowCommentClick = () => {
+  console.log("Showing comments");
+  showComments.value = !showComments.value;
+}
 const props = defineProps({
   username: String,
   userId: Number,
   avatarSrc: String,
-  post: String
+  post: String,
+  comments: Array,
+  likes: Number,
+  retweets: Number
+});
+
+const hasComments = computed(() => {
+  return props.comments.length > 0;
+});
+
+const interactions = computed( ()=> {
+  const comments = props.comments.length;
+  console.log(comments, props.likes, props.retweets);
+  return comments + props.likes + props.retweets;
 });
 
 onMounted( () => {
@@ -48,6 +81,10 @@ onMounted( () => {
     font-weight: bold;
     margin-right: 8px;
     color: white;
+  }
+  .interactions {
+    font-weight: bold;
+    margin-top: 8px;
   }
 }
 </style>
