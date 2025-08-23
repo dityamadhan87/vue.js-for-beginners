98400d71c032b74733dd9fb89faed94569e00cb9
Raditya Rahmadhani <edogawaradit@gmail.com>
2025-08-23 14:07:43 +0700

    Update Cypress configuration, add homepage tests, and implement TheButton component tests

---
+++ src/components/__tests__/TheButton.spec.js
@@ -0,0 +1,24 @@
+import { expect, describe, it } from 'vitest'
+import component from '../atoms/TheButton.vue'
+import {mount} from "@vue/test-utils";
+
+describe('TheButton.vue', () => {
+    describe('when mounted', () => {
+        it('renders properly', () => {
+            const wrapper = mount(component, {});
+            expect(wrapper.html()).toContain('button');
+        });
+        it('default to light theme', () => {
+            const wrapper = mount(component, {});
+            expect(wrapper.classes()).toContain('light');
+        });
+        it('accepts dark theme', () => {
+            const wrapper = mount(component, {
+                props: {
+                    theme: 'dark'
+                }
+            });
+            expect(wrapper.classes()).toContain('dark');
+        });
+    });
+});
